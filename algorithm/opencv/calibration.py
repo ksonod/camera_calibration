b9fd996f2f45414ddb6e120a1e8d75877ad616f6
Kotaro Sonoda <sonoda.ktr@gmail.com>
2024-01-01 10:10:22 +0100

    fix: Make OpenCV reprojection error consistent with MATLAB.

--- algorithm/opencv/calibration.py
+++ algorithm/opencv/calibration.py
@@ -11,50 +11,67 @@ def calibrate_with_opencv(config: dict, img_file_list: list):
 
     num_img_files = len(img_file_list)
 
-    # termination criteria
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((np.prod(config["checkerboard"]["num_corners"]), 3), np.float32)
+    objp = np.zeros(
+        (np.prod(config["checkerboard"]["num_corners"]), 3),
+        np.float32
+    )  # Object points in 3D
     objp[:, :2] = np.mgrid[
                   0:config["checkerboard"]["num_corners"][0],
                   0:config["checkerboard"]["num_corners"][1]
-                  ].T.reshape(-1, 2) * config["checkerboard"]["checker_size"]
-    # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3d point in real world space
-    imgpoints = []  # 2d points in image plane.
-    gray_img_stack = []
+                  ].T.reshape(-1, 2) * config["checkerboard"]["checker_size"]  # Z values are always 0.
+
+    points3d = []  # 3D point in real world space
+    points2d = []  # 2D points in image plane.
+
     for idx, img_file in enumerate(img_file_list):
         img = np.array(Image.open(img_file))
-        gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-        # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, config["checkerboard"]["num_corners"], None)
-        # If found, add object points, image points (after refining them)
-        if ret:
-            gray_img_stack.append(gray)
-            objpoints.append(objp)
-            corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-            imgpoints.append(corners2)
+        gray_img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+        detected, detected_corners = cv.findChessboardCorners(
+            image=gray_img, patternSize=config["checkerboard"]["num_corners"], corners=None
+        )
+
+        if detected:
+            points3d.append(objp)
+            detected_corners_subpix = cv.cornerSubPix(
+                image=gray_img, corners=detected_corners, winSize=(11, 11), zeroZone=(-1, -1),
+                criteria=(cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)  # Termination criteria
+            )
+            points2d.append(detected_corners_subpix)
 
             if config["checkerboard"]["show_figure"]:
-                cv.drawChessboardCorners(img, config["checkerboard"]["num_corners"], corners2, ret)
-                cv.imshow('img', img)
+                cv.drawChessboardCorners(
+                    image=img,
+                    patternSize=config["checkerboard"]["num_corners"],
+                    corners=detected_corners_subpix,
+                    patternWasFound=detected
+                )
+                cv.imshow(winname='img', mat=img)
                 cv.waitKey()
+        else:
+            print(f"Corners are not detected ({img_file.name})")
     cv.destroyAllWindows()
 
-    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    ret, K, distortion_params, rvecs, tvecs = cv.calibrateCamera(
+        objectPoints=points3d, imagePoints=points2d, imageSize=gray_img.shape[::-1], cameraMatrix=None, distCoeffs=None
+    )
 
     np.set_printoptions(precision=3, suppress=True)
-    print(f"- Intrinsic parameters : \n{mtx}\n")
+    print(f"- Intrinsic parameters : \n{K}\n")
 
     print("- Extrinsic parameters")
 
     reprojection_error = []
-    for i in range(num_img_files):
-        projected_points2d, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    num_valid_files = len(points2d)  # Number of files where checkerboard corners are detected.
+    for i in range(num_valid_files):
+        projected_points2d, _ = cv.projectPoints(
+            objectPoints=points3d[i], rvec=rvecs[i], tvec=tvecs[i], cameraMatrix=K, distCoeffs=distortion_params
+        )
 
+        err = projected_points2d - points2d[i]
         # averaged over all the corners detected in a single image
         reprojection_error.append(
-            cv.norm(imgpoints[i], projected_points2d, cv.NORM_L2) / len(projected_points2d)
+            np.mean(np.sqrt(err[:, :, 0] ** 2 + err[:, :, 1] ** 2))
         )
 
         print(f"{img_file_list[i].name} | Reprojection error = {reprojection_error[-1]:.5f}")
@@ -66,7 +83,6 @@ def calibrate_with_opencv(config: dict, img_file_list: list):
     print("- Mean reprojection error")
     print(f" Overall: {np.mean(reprojection_error):.5f}")  # Averaging reprojection errors over all images
 
-
     plt.figure()
     plt.bar(
         np.arange(num_img_files), reprojection_error, color="blue", alpha=0.5
@@ -77,4 +93,3 @@ def calibrate_with_opencv(config: dict, img_file_list: list):
     plt.xlabel("Images")
     plt.ylabel("Mean reprojection error (pixel)")
     plt.show()
-
